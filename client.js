b8d0cca05ed203895b03f7b2b618ecbd4243e32e
Marta Jopp <martapeterson@gmail.com>
2018-04-26 21:28:24 -0500

    seconds and minutes

--- client.js
+++ client.js
@@ -1,11 +1,22 @@
 console.log('sourced')
 
+const secondHand = docuent.querySelector('.second-hand');
+const minsHand = docuent.querySelector('.min-hand');
 
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
     console.log(seconds)
+    const secondsDegrees = ((seconds/60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins/60) * 360) + ((seconds/60)*6) + 90;
+    minsHand.style.transform = `rotate(${minsDegees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour/12)* 360) + ((mins/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000)
